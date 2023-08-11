1da5d9060fec7835d44bb9df4d27afa4422e4ac9
NinaWie <wnina@student.ethz.ch>
2023-08-11 16:22:49 +0200

    add visualization functions and refactor (scripts folder)

--- ebike_city_tools/visualize.py
+++ ebike_city_tools/visualize.py
@@ -2,6 +2,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import os
 
@@ -16,7 +17,14 @@ def visualize_graph(G):
 
     nodes = nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color="indigo")
     edges = nx.draw_networkx_edges(
-        G, pos, node_size=node_sizes, arrowstyle="->", arrowsize=10, edge_color=edge_colors, edge_cmap=cmap, width=2,
+        G,
+        pos,
+        node_size=node_sizes,
+        arrowstyle="->",
+        arrowsize=10,
+        edge_color=edge_colors,
+        edge_cmap=cmap,
+        width=2,
     )
     # set alpha value for each edge
     for i in range(M):
@@ -33,8 +41,8 @@ def visualize_graph(G):
 
 def plot_graph(G, directed=True, hw=0.05, weight="weight"):
     """
-    Plot a graph G, 
-        with edges coloured according to the edge attribute weight, 
+    Plot a graph G,
+        with edges coloured according to the edge attribute weight,
         as arrows if directed=True,
         with arrow head width hw
     """
@@ -92,17 +100,21 @@ def pareto_plot_sp(res, out_path):
         "spanning_random": "Minimal spanning tree forms bike network",
         "betweenness": "Bike lanes found by minimum betweenness centrality [1]",
         "optim_betweenness": "Randomly swap edges to improve [1]",
-        "original": "No bike lanes (original car network)"
+        "original": "No bike lanes (original car network)",
     }
     res["Method"] = res["Method"].map(method_mapping)
     # sort
-    res = res.set_index(["Method"]).loc[
-        [
-            'Minimal spanning tree forms bike network',
-            'Bike lanes found by minimum betweenness centrality [1]',
-            'Randomly swap edges to improve [1]'
+    res = (
+        res.set_index(["Method"])
+        .loc[
+            [
+                "Minimal spanning tree forms bike network",
+                "Bike lanes found by minimum betweenness centrality [1]",
+                "Randomly swap edges to improve [1]",
+            ]
         ]
-    ].reset_index()
+        .reset_index()
+    )
 
     metric = "sp_length"
     xlim = 6.4
@@ -111,7 +123,7 @@ def pareto_plot_sp(res, out_path):
     sns.scatterplot(data=res, x="bike_" + metric, y="car_" + metric, hue="Method", s=100, palette="Set2")
     plt.plot([0, xlim], [car_optim, car_optim], label=method_mapping["original"], linestyle="--", color="red")
     plt.plot([bike_optim, bike_optim], [0, ylim], label=method_mapping["full_random"], linestyle="--", color="green")
-    plt.legend(title="Method", bbox_to_anchor=(1,0.8))
+    plt.legend(title="Method", bbox_to_anchor=(1, 0.8))
     plt.ylabel("Avg. shortest path length (CAR)")
     plt.xlabel("Avg. shortest path length (BIKE)")
     plt.xlim(0, xlim)
@@ -119,3 +131,54 @@ def pareto_plot_sp(res, out_path):
     plt.tight_layout()
     plt.savefig(os.path.join(out_path, "pareto_sp.jpg"))
     plt.show()
+
+
+def visualize_runtime_dependency(path="outputs/runtime.csv", out_path="outputs"):
+    """Plot the runtime for initialization and optimization"""
+    runtime = pd.read_csv(path)
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.scatter(runtime["od_size"], runtime["time init"], c=runtime["edges"])
+    plt.xlabel("Number of s-t-paths (size of OD matrix)")
+    plt.ylabel("LP initialization runtime [s]")
+    plt.colorbar(label="Number of edges")
+    plt.subplot(1, 2, 2)
+    plt.scatter(runtime["nodes"], runtime["time optimize"], c=runtime["edges"])
+    plt.xlabel("Number of s-t-paths (size of OD matrix)")
+    plt.ylabel("Optimization runtime [s]")
+    plt.colorbar(label="Number of edges")
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_path, "runtime_analysis.png"))
+
+
+def visualize_od_dependency(path="outputs/od_dependency.csv", out_path="outputs"):
+    od_dependency = pd.read_csv(path)
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.scatter(od_dependency["od reduction"], od_dependency["runtime_init"], c="blue", label="init")
+    plt.scatter(od_dependency["od reduction"], od_dependency["runtime_optim"], c="orange", label="optim")
+    plt.ylabel("runtime")
+    plt.xlabel("OD reduction factor")
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.scatter(od_dependency["od reduction"], od_dependency["bike sp lengths"], c="blue", label="bike")
+    plt.scatter(od_dependency["od reduction"], od_dependency["car sp lengths"], c="orange", label="car")
+    plt.ylabel("SP lengths")
+    plt.xlabel("OD reduction factor")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_path, "od_dependency_analysis.png"))
+
+    od_dependency.groupby("od reduction").agg(
+        {
+            "car sp reachable": "mean",
+            "bike sp reachable": "mean",
+            "bike sp lengths": ["mean", "std"],
+            "car sp lengths": ["mean", "std"],
+        }
+    ).to_csv(os.path.join(out_path, "od_nonreachable.csv"))
+
+
+if __name__ == "__main__":
+    visualize_runtime_dependency()
+    visualize_od_dependency()
