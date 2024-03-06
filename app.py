2a01b4e35b3ea1119d0e1649352d273f937bd5a8
NinaWie <wnina@student.ethz.ch>
2024-03-06 15:22:45 +0100

    wip: python app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import os
 import numpy as np
-
+import geopandas as gpd
 import pyproj
 import networkx as nx
 
@@ -12,9 +13,21 @@ from ebike_city_tools.graph_utils import (
     clean_street_graph_directions,
     lane_to_street_graph,
 )
+from shapely.geometry import Polygon
 from ebike_city_tools.iterative_algorithms import topdown_betweenness_pareto, betweenness_pareto
 from ebike_city_tools.od_utils import extend_od_circular
 from ebike_city_tools.optimize.round_optimized import ParetoRoundOptimize
+from ebike_city_tools.app_utils import (
+    PATH_DATA,
+    generate_od_nodes,
+    generate_od_geometry,
+    get_expected_time,
+    compute_nr_variables,
+)
+
+maxspeed_fill_val = 50
+include_lanetypes = ["H>", "H<", "M>", "M<", "M-"]
+fixed_lanetypes = ["H>", "<H"]
 
 ROUNDING_METHOD = "round_bike_optimize"
 IGNORE_FIXED = True
@@ -36,49 +49,120 @@ algorithm_dict = {
 app = Flask(__name__)
 CORS(app, origins=["*", "null"])  # allowing any origin as well as localhost (null)
 
+# load main nodes and edges that will be used for any graph
+CRS = 2056
+zurich_nodes = gpd.read_file(os.path.join(PATH_DATA, "street_graph_nodes.gpkg")).to_crs(CRS).set_index("osmid")
+zurich_edges = gpd.read_file(os.path.join(PATH_DATA, "street_graph_edges.gpkg")).to_crs(CRS)
+# some preprocessing
+zurich_edges = clean_street_graph_multiedges(zurich_edges)
+zurich_edges = clean_street_graph_directions(zurich_edges)
 
-@app.route("/optimize", methods=["POST"])
-def optimize():
+# Dictionary storing the graphs and OD matrices per project. TODO: replace with database
+project_dict = {}
 
-    data = request.get_json(force=True)
-    algorithm = request.args.get("algorithm", "optimize")
-    ratio_bike_edges = float(request.args.get("bike_ratio", "0.4"))
 
-    # ARGUMENTS --> TODO: get from request args instead
-    remove_multistreets = True
-    target_crs = 2056
-    maxspeed_fill_val = 50
-    include_lanetypes = ["H>", "H<", "M>", "M<", "M-"]
-    fixed_lanetypes = ["H>", "<H"]
+def create_new_project_id():
+    key_list = project_dict.keys()
+    if len(key_list) == 0:
+        return 0
+    else:
+        return max(key_list) + 1
 
-    desired_edge_count = int(ratio_bike_edges * G_lane.number_of_edges())
 
-    # TODO: extract nodes and edges from transmitted data
-    # street_graph_nodes, street_graph_edges = ...
-    # od = ...
-    if remove_multistreets:
-        street_graph_edges = clean_street_graph_multiedges(street_graph_edges)
-        print("removed multiedges from street graph", len(street_graph_edges))
-        street_graph_edges = clean_street_graph_directions(street_graph_edges)
-        print("removed bidirectional edges from street graph", len(street_graph_edges))
+@app.route("/construct_graph", methods="POST")
+def generate_input_graph():
+    """
+    Generate an input graph from a bounding polygon.
+    Currently only using pre-loaded graph data from Zurich
 
-    # convert to lane graph
+    Notes:
+    - # the input data is a list of projected 2D coordinates that can be transformed into a Polygon, e.g.
+        [[2678000.0, 1247000.0], [2678000.0, 1250000.0], [2681000.0, 1250000.0], [2681000.0, 1247000.0]]
+    - In future versions, this should be extended to constructing arbitrary graphs from OSM data.
+    - There are two modes for creating the OD matrix, one simply based on the nodes (named "fast") and one based on the
+        geometries ("slow")
+    """
+    # get input arguments: polygons for bounding the region and OD mode
+    bounds_polygon = request.get_json(force=True)
+    od_creation_mode = request.args.get("odmode", "fast")
+
+    try:
+        area_polygon = Polygon(bounds_polygon, crs=CRS)
+    except ValueError:
+        return (jsonify("Coordinates have wrong format. Check the documentation."), 400)
+
+    # restrict graph to Polygon
+    zurich_nodes_area = zurich_nodes.sjoin(area_polygon)
+    zurich_edges_area = zurich_edges.sjoin(area_polygon)
+    # if the graph is empty, return message
+    if len(zurich_edges_area) == 0:
+        return (jsonify("No edges found in this area. Try with other coordinates."), 400)
+
+    # create OD matrix
+    if od_creation_mode == "fast":
+        od = generate_od_nodes(zurich_nodes_area)
+    elif od_creation_mode == "slow":
+        od = generate_od_geometry(area_polygon, zurich_nodes_area)
+    else:
+        return (jsonify("Wrong value for odmode argument. Must be one of {slow, fast}"), 400)
+
+    # create graph
     G_lane = street_to_lane_graph(
-        street_graph_nodes,
-        street_graph_edges,
+        zurich_nodes_area,
+        zurich_edges_area,
         maxspeed_fill_val=maxspeed_fill_val,
         include_lanetypes=include_lanetypes,
         fixed_lanetypes=fixed_lanetypes,
-        target_crs=target_crs,
+        target_crs=CRS,
     )
 
-    # TODO: use only Zurich and load OD from file
+    # we need to extend the OD matrix to guarantee connectivity of the car network
     od = od[od["s"] != od["t"]]
-    # reduce OD matrix to nodes that are in G_lane
     node_list = list(G_lane.nodes())
     od = od[(od["s"].isin(node_list)) & (od["t"].isin(node_list))]
+    od_matrix_area_extended = extend_od_circular(od, node_list)
+
+    # estimate runtime
+    nr_variables = compute_nr_variables(G_lane.number_of_edges(), len(od_matrix_area_extended))
+    runtime_min = get_expected_time(nr_variables)
+
+    # save graph and OD matrix with a project ID
+    project_id = create_new_project_id()
+    project_dict[project_id] = {"lane_graph": G_lane, "od": od, "bounds": area_polygon}
+
+    return (jsonify({"project_id": project_id, "variables": nr_variables, "expected_runtime": runtime_min}), 200)
+
+
+@app.route("/optimize", methods=["POST"])
+def optimize():
+    """
+    Run optimization on graph that was previously created
+    Request arguments:
+        algorithm: One of {optimize, betweenness_biketime, betweenness_cartime, betweenness_topdown} - see paper
+        ratio_bike_edges: How many lanes should preferably become bike lanes? Defaults to 0.4 --> 40% of all lanes
+        optimize_every_x: How often to re-run the optimization. The more often, the better, but also much slower
+        car_weight: Weighting of the car travel time in the objective function. Should be something between 0.1 and 10
+        bike_safety_penalty: factor by how much the perceived bike travel time increases if cycling on car lane.
+            Defaults to 2, i.e. the perceived travel time on a car lane is twice as much as the one on a bike lane
+    """
+    project_id = request.args.get("project_id")
+    algorithm = request.args.get("algorithm", "optimize")
+    ratio_bike_edges = float(request.args.get("bike_ratio", "0.4"))
+    optimize_every_x = float(request.args.get("optimize_frequency", "30"))
+    car_weight = float(request.args.get("car_weight", "2"))
+    shared_lane_factor = float(request.args.get("bike_safety_penalty", "2"))
+
+    if project_id not in project_dict.keys():
+        return (jsonify("Project not find. Call `construct_graph` first to start a new project"), 400)
+
+    G_lane = project_dict[project_id]["lane_graph"]
+    od = project_dict[project_id]["od"]
+
+    # compute the absolute number of bike lanes that are desired
+    desired_edge_count = int(ratio_bike_edges * G_lane.number_of_edges())
 
     if "betweenness" in algorithm:
+
         print(f"Running betweenness algorithm {algorithm}")
         # get algorithm method
         algorithm_func, kwargs = algorithm_dict[algorithm]
@@ -90,22 +174,22 @@ def optimize():
             od_matrix=od,
             weight_od_flow=WEIGHT_OD_FLOW,
             fix_multilane=FIX_MULTILANE,
+            shared_lane_factor=shared_lane_factor,
             save_graph_path=None,
             return_graph_at_edges=desired_edge_count,
             **kwargs,
         )
     else:
-        # FOR OPTIMIZATION
-        # extend OD matrix because otherwise we get disconnected car graph
-        od = extend_od_circular(od, node_list)
+        # potentially only extend OD matrix for the optimzation algorithm and not for the betweenness algorithm
+        # od = extend_od_circular(od, node_list)
 
         opt = ParetoRoundOptimize(
             G_lane.copy(),
             od.copy(),
-            optimize_every_x=OPTIMIZE_EVERY_K,
-            car_weight=CAR_WEIGHT,
+            optimize_every_x=optimize_every_x,
+            car_weight=car_weight,
             sp_method=SP_METHOD,
-            shared_lane_factor=SHARED_LANE_FACTOR,
+            shared_lane_factor=shared_lane_factor,
             weight_od_flow=WEIGHT_OD_FLOW,
             valid_edges_k=0,
         )
@@ -114,6 +198,8 @@ def optimize():
 
     # convert to pandas datafrme
     result_graph_edges = nx.to_pandas_edgelist(result_graph)
+
+    # TODO: modify output to return only geometries with bike/car label
     return jsonify(result_graph_edges)
 
 
---
+++ ebike_city_tools/app_utils.py
@@ -0,0 +1,71 @@
+import os
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+PATH_DATA = "../street_network_data/zurich/"
+CRS = 2056
+
+# load the whole-city trip and construct origin and destination geometry
+trips_microcensus = gpd.read_file(os.path.join(PATH_DATA, "raw_od_matrix", "trips_mc_cleaned_proj.gpkg"))
+trips_microcensus["geom_destination"] = gpd.points_from_xy(
+    x=trips_microcensus["end_lng"], y=trips_microcensus["end_lat"]
+)
+trips_microcensus["geom_origin"] = gpd.points_from_xy(
+    x=trips_microcensus["start_lng"], y=trips_microcensus["start_lat"]
+)
+
+# load prebuilt OD matrix
+od_whole_zurich_nodes = pd.read_csv("../street_network_data/zurich")
+
+
+def get_expected_time_linear(nr_variables, coef=2.17235844e-05, intercept=-15.15954725242839):
+    """Fit linear function to runtime from number of variables"""
+    return nr_variables * coef + intercept
+
+
+def get_expected_time(nr_variables):
+    """Computes expected runtime (in min) from number of variables"""
+    return 0.8 * np.exp(nr_variables / 1000000 * 1.6)
+
+
+def compute_nr_variables(nr_edges, od_len):
+    """Compute number of variables"""
+    nr_variables = 3 * (od_len * nr_edges) + 2 * nr_edges
+    return nr_variables
+
+
+def generate_od_nodes(nodes):
+    assert nodes.index.name == "osmid"
+    nodes_in_area = nodes.index.unique()
+    od_in_area = od_whole_zurich_nodes[
+        (od_whole_zurich_nodes["s"].isin(nodes_in_area)) & (od_whole_zurich_nodes["t"].isin(nodes_in_area))
+    ]
+    return od_in_area
+
+
+def generate_od_geometry(area_polygon, nodes):
+    # restrict trips to the ones crossing the area
+    trips = trips_microcensus.sjoin(area_polygon)
+
+    # get the closest nodes to the respective destination
+    trips.set_geometry("geom_destination", inplace=True, crs=CRS)
+    trips.to_crs(nodes.crs, inplace=True)
+    trips = trips.sjoin_nearest(nodes, distance_col="dist_destination", how="left", lsuffix="", rsuffix="destination")
+    trips.rename(columns={"osmid": "osmid_destination"}, inplace=True)
+
+    # trips["geom_origin"].apply(wkt.loads)
+    trips.set_geometry("geom_origin", inplace=True, crs=CRS)
+    trips.to_crs(nodes.crs, inplace=True)
+    trips.drop(["geom_destination"], axis=1, inplace=True)
+
+    # get the closest nodes to the respective origin
+    trips = trips.sjoin_nearest(nodes, distance_col="dist_origin", how="left", lsuffix="", rsuffix="origin")
+    trips.rename(columns={"osmid": "osmid_origin"}, inplace=True)
+    od_in_area = (
+        trips.groupby(["osmid_origin", "osmid_destination"])
+        .agg({"count": "sum"})
+        .reset_index()
+        .rename(columns={"osmid_origin": "s", "osmid_destination": "t", "count": "trips"})
+    )
+    return od_in_area
