05d0cdcc80567036d221cecd6d68c5daa90e57cb
NinaWie <wnina@student.ethz.ch>
2024-11-28 23:30:57 +0100

    script adaptations for rebuttal experiments

--- scripts/integer_vs_linear.py
+++ scripts/integer_vs_linear.py
@@ -13,31 +13,49 @@ from ebike_city_tools.optimize.round_optimized import ParetoRoundOptimize
 
 OUT_PATH = "outputs"
 os.makedirs(OUT_PATH, exist_ok=True)
-NR_ITERS = 50
+os.makedirs("outputs/int_vs_lin_graphs", exist_ok=True)
+NR_ITERS = 15
 OPTIMIZE_EVERY_K = 10
 shared_lane_factor = 2
 SP_METHOD = "od"
 WEIGHT_OD_FLOW = False
+od_factor = 0.01
 
 if __name__ == "__main__":
     np.random.seed(42)
     res_df = []
     for i in range(NR_ITERS):
         # only run on graph of size 30 to make it feasible
-        for size in [30]:
-            G_lane = random_lane_graph(size)
+        for size in [80, 60]:
+            neighbor_p = np.random.choice(["[0.6, 0.3, 0.1]", "[0.7, 0.2, 0.1]", "[0.8, 0.1, 0.1]"])
+            G_lane = random_lane_graph(size, neighbor_p=eval(neighbor_p))
+            nx.write_adjlist(G_lane, f"outputs/int_vs_lin_graphs/graph_{i}_{size}.gpickle")
+
+            centralities = list(nx.edge_betweenness_centrality(G_lane).values())
+            bet_mean, bet_std = np.mean(centralities), np.std(centralities)
+
+            cluster = list(nx.clustering(nx.DiGraph(G_lane)).values())
+            cluster_mean, cluster_std = np.mean(cluster), np.std(cluster)
+            print("----------")
+            print("Graph", G_lane.number_of_nodes(), G_lane.number_of_edges())
+            print("betweenness und clustering", bet_mean, bet_std, cluster_mean, cluster_std, neighbor_p)
+
             G = lane_to_street_graph(G_lane)
             # test for graphs with OD matrix of 10% of the all-pairs SP
-            for od_factor in [0.1]:
-                # define graph
-                od = make_fake_od(size, int(od_factor * size**2), nodes=G.nodes)
-                od = extend_od_circular(od, list(G_lane.nodes()))
+            # define graph
+            od = make_fake_od(size, int(od_factor * size**2), nodes=G.nodes)
+            od = extend_od_circular(od, list(G_lane.nodes()))
+
+            for OPTIMIZE_EVERY_K in [10, 20]:
 
                 # run for several car weights to get a pareto frontier also for the integer problem
-                for car_weight in [0.1, 0.5, 0.75, 1, 2, 4, 8]:
+                for car_weight in [0.5, 0.75, 1, 2, 4, 8]:
                     for integer_problem, name in zip([True, False], ["integer", "linear"]):
                         # transform the graph layout into travel times, including gradient and penalty factor for using
                         if integer_problem:
+                            if OPTIMIZE_EVERY_K != 10:
+                                # need to do linear problem only ones
+                                continue
                             optim = Optimizer(
                                 graph=G.copy(),
                                 od_matrix=od,
@@ -89,10 +107,15 @@ if __name__ == "__main__":
                                     "iter": i,
                                     "name": name,
                                     "nodes": size,
-                                    "edges": G.number_of_edges(),
+                                    "edges": G_lane.number_of_edges(),
+                                    "betweenness_mean": bet_mean,
+                                    "betweenness_std": bet_std,
+                                    "cluster_mean": cluster_mean,
+                                    "cluster_std": cluster_std,
                                     "od_size": len(od),
                                     "car_weight": car_weight,
                                     "opt_value": obj_value,
+                                    "optimize_every": OPTIMIZE_EVERY_K,
                                     "time init": toc - tic,
                                     "time optimize": toc_finished - toc,
                                 }
