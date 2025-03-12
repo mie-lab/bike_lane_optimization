d91e1fe668d74a22e74348a766cf578119b4bb4c
NinaWie <wnina@student.ethz.ch>
2025-03-12 09:45:51 +0100

    cleanups

--- run_server.sh
+++ run_server.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Define the code snippet to be added
-
-# code_to_append='
-# def create_app():
-#     return app
-# '
-
-# Append the code to the end of the prod/app.py file
-echo "$code_to_append" >> app.py
-
-
 # Activate virtual environment
 source env/bin/activate
 
